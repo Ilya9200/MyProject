8705553d34c4b5945b4bead15c494b5bfbed3e00
admin <dasdasd>
2018-07-25 18:57:39 +0400

    asdasd

---
+++ P_Dasdasd_Scripts.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EleWise.ELMA.API;
+using EleWise.ELMA.Model.Common;
+using EleWise.ELMA.Model.Entities;
+using EleWise.ELMA.Model.Managers;
+using EleWise.ELMA.Model.Types.Settings;
+using EleWise.ELMA.Model.Entities.ProcessContext;
+using Context = EleWise.ELMA.Model.Entities.ProcessContext.P_Dasdasd;
+
+namespace EleWise.ELMA.Model.Scripts
+{
+    
+    /// <summary>
+    /// Модуль сценариев процесса "dasdasd"
+    /// </summary>
+    /// <example> 
+    /// <![CDATA[
+    /// >>>>>>>>>>>>>>>ВАЖНАЯ ИНФОРМАЦИЯ!!!<<<<<<<<<<<<<<<
+    /// Данный редактор создан для работы с PublicAPI. 
+    /// PublicAPI предназначен для разработки сценариев ELMA.
+    /// Например, с помощью PublicAPI можно добавить комментарий к документу:
+    /// //Загружаем документ
+    /// var doc = PublicAPI.Docflow.Document.Load(56);
+    /// //Добавляем комментарий
+    /// PublicAPI.Docflow.Document.AddComment(doc, "тут ваш комментарий");
+    /// 
+    /// Более подробно про PublicAPI вы можете узнать тут: http://www.elma-bpm.ru/kb/article-642ApiRoot.html
+    /// 
+    /// Если же вам нужна более серьёзная разработка, выходящая за рамки PublicAPI, используйте
+    /// сторонние редакторы кода, такие как SharpDevelop и VisualStudio.
+    /// Информацию по запуску кода в стороннем редакторе вы можете найти тут:
+    /// http://www.elma-bpm.ru/kb/article-837.html
+    /// ]]>
+
+    /// </example>
+    public partial class P_Dasdasd_Scripts : EleWise.ELMA.Workflow.Scripts.ProcessScriptBase<Context>
+    {
+
+        
+
+    }
+
+}
