33188965d4456f66be4d52d762fedb050aeeae59
Hlebnikov Ilya <hlebnikov@elewise.com>
2018-07-26 16:03:49 +0400

    фывывфыв

--- P_Uluchshenie_Scripts.cs
+++ P_Uluchshenie_Scripts.cs
@@ -33,7 +33,7 @@ namespace EleWise.ELMA.Model.Scripts
         {
         	var matrixItem = ((IBPMNProcess) context.ImprovedProcessHeader.Published.CastAsRealType()).ResponsibilityMatrix.FirstOrDefault(x => x.ResponsibilityLevel == ResponsibilityLevel.Owner);        	
         	if (matrixItem != null && matrixItem.WorkerId.HasValue)
-        	{
+        	{фывфывфывфы
         		var settings = (EntityUserSettings)context.GetSettingsFor(c => c.ProcessOwner);
             	settings.Workers.Clear();
             	settings.Workers.Add(new Worker { WorkerId = matrixItem.WorkerId.Value, WorkerType = matrixItem.WorkerType });
