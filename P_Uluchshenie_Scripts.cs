35769f714ce7a42192e14a37c74cf34f460310bb
Hlebnikov Ilya <hlebnikov@elewise.com>
2018-07-26 16:04:29 +0400

    фывфывфывфыв

--- P_Uluchshenie_Scripts.cs
+++ P_Uluchshenie_Scripts.cs
@@ -1,8 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+фывфывфы
 using EleWise.ELMA.Extensions;
 using EleWise.ELMA.Model.Common;
 using EleWise.ELMA.Model.Entities;
