85aa441da2c70151f002ffac6e2f92f28b0ca87d
admin <dasdas>
2018-07-30 12:17:53 +0400

    yyyy

--- P_Uluchshenie_Scripts.cs
+++ P_Uluchshenie_Scripts.cs
@@ -54,7 +54,7 @@ namespace EleWise.ELMA.Model.Scripts
              	swimlaneExecutor.User = admin;
 
         		// исполнитель по-умолчанию
-        		context.Worker = admin;asdasd
+        		context.Worker = admin;asdasdsadasd
         	}
 
         }       
